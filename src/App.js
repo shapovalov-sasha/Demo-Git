568e120d07df53e9475d22c5a22bac1be60aef2a
shapovalov-sasha <shapovalovalexandru@gmail.com>
2022-06-22 22:03:05 +0300

    shasha_1

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <p>Sasha</p>
         <a
           className="App-link"
           href="https://reactjs.org"
